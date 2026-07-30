f64fc58b2b681a03377a05bd48956bc43586683d
Hariche Wissem <wissemhariche59@gmail.com>
2026-07-30 19:41:07 +0200

    Avancée des TP en Javascript

--- Partie Javascript/Les TP/Partie 2/TP2/index.js
+++ Partie Javascript/Les TP/Partie 2/TP2/index.js
@@ -1,7 +1,20 @@
 // Créer un événément au scroll
 
+/*window.addEventListener ("scroll", (e) => {
+    console.log(window.scrollY); // Connaitre niveau de scroll (window.scrollY)
+})*/
+
 // Cacher la navbar si l'utilisateur comment à descendre et la sortir quand il remonte
 
-// Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
+const navbar = document.querySelector("nav");
+let scroll = window.scrollY;
 
-// Connaitre niveau de scroll (window.scrollY)
+window.addEventListener ("scroll", (e) => {
+    if (window.scrollY < scroll) {
+        navbar.style.top = "-60px";
+    } else {
+        navbar.style.top = "0px";
+    }
+
+    scroll = window.scrollY; // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
+});
--- Partie Javascript/Les TP/Partie 2/TP3/script.js
+++ Partie Javascript/Les TP/Partie 2/TP3/script.js
@@ -1,9 +1,42 @@
 // Créer un événement au scroll
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+const popup = document.querySelector(".pop-up");
 
-// Faire apparaitre l'image de la partie improvise
+let isButtonPressed = 0;
+// const boutonClique = document.querySelector("button-container button:active");
+const bouton = document.querySelector(".fa-times-circle");
 
-// Faire apparaitre la popup quand on est en bas du site
+window.addEventListener ("click", (e) => {
+    if (e.target === isButtonPressed) {
+        isButtonPressed = 1;
+        popup.style.opacity = "0";
+        popup.style.transform = "translateX(400px)";
+    }
+});
 
-// Bonus : quand on clicke sur la popup elle disparait pour toujours
+window.addEventListener ("scroll", (e) => {
+    // Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+    if (window.scrollY > 50) {
+        navbar.style.height = "45px";
+    } else {
+        navbar.style.height = "90px";
+    }
+
+    // Faire apparaitre l'image de la partie improvise
+    if (window.scrollY >= 85) {
+        imgImprovise.style.opacity = "1";
+        imgImprovise.style.transform = "translateX(0px)";
+    } else {
+        imgImprovise.style.opacity = "0";
+        imgImprovise.style.transform = "translateX(-200px)";
+    }
+
+    // Faire apparaitre la popup quand on est en bas du site
+    if (((window.innerHeight + Math.round(window.scrollY)) >= document.body.offsetHeight) && (isButtonPressed == 0)) {
+        popup.style.opacity = "1";
+        popup.style.transform = "translateX(0px)";
+    } else {
+        popup.style.opacity = "0";
+        popup.style.transform = "translateX(400px)";
+    }
+});
